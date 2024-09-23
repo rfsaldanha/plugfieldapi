e60f6bd92b46ef0bc8d3e17a6c547e4d3c53aa19
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-23 13:03:17 +0200

    Function to retrieve daily data

---
+++ R/data_daily.R
@@ -0,0 +1,27 @@
+data_daily <- function(deviceId, begin, end){
+  # Try to login
+  if(!check_login()){
+    login()
+  }  
+  
+  # Request specification
+  req <- httr2::request(base_url = server_url) |>
+    httr2::req_url_path("data/daily") |>
+    httr2::req_url_query(device = deviceId) |>
+    httr2::req_url_query(begin = begin) |>
+    httr2::req_url_query(end = end) |>
+    httr2::req_headers("Content-Type" = "application/json") |>
+    httr2::req_headers("Authorization" = the$access_token) |>
+    httr2::req_headers("x-api-key" = the$x_api_key) |>
+    httr2::req_throttle(rate = throttle_rate, realm = server_url) |>
+    httr2::req_retry(max_tries = retry_max_tries)
+  
+  # Request perform
+  resp <- req |> 
+    httr2::req_perform()
+
+  # Response to list
+  res <- resp |> httr2::resp_body_json()
+
+  return(res)
+}
