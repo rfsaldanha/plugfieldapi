14e4e8c832971e1322d2c704f412c082831c1026
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-23 12:48:15 +0200

    Function to retrieve specific device info

--- R/device_info.R
+++ R/device_info.R
@@ -1,12 +1,13 @@
-device_list <- function(){
+device_info <- function(deviceId){
   # Try to login
   if(!check_login()){
     login()
-  }
-
+  }  
+  
   # Request specification
   req <- httr2::request(base_url = server_url) |>
     httr2::req_url_path("device") |>
+    httr2::req_url_path_append(deviceId) |>
     httr2::req_headers("Content-Type" = "application/json") |>
     httr2::req_headers("Authorization" = the$access_token) |>
     httr2::req_headers("x-api-key" = the$x_api_key) |>
@@ -14,18 +15,11 @@ device_list <- function(){
       httr2::req_retry(max_tries = retry_max_tries)
   
   # Request perform
-  resps <- httr2::req_perform_iterative(
-    req |> httr2::req_url_query(limit = 1),
-    next_req = httr2::iterate_with_offset(
-      "page_index",
-      resp_pages = function(resp) httr2::resp_body_json(resp)$pagination$totalPages
-    ),
-    max_reqs = Inf
-  )
+  resp <- req |> 
+    httr2::req_perform()
 
-  # Responses to list
-  res <- resps |> httr2::resps_successes() |>
-    httr2::resps_data(\(resp) httr2::resp_body_json(resp))
+  # Response to list
+  res <- resp |> httr2::resp_body_json()
 
   return(res)
 }
