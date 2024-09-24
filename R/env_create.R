6019a7b6611de368ffc3e6d9f369fdb55ce6317e
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-24 16:04:39 +0200

    Change file name and split.

---
+++ R/env_create.R
@@ -0,0 +1,2 @@
+# Create enviroment to handle login information
+the <- new.env(parent = emptyenv())
---
+++ R/options.R
@@ -0,0 +1,9 @@
+# Server base url
+server_url <- "https://prod-api.plugfield.com.br"
+
+# API limits
+throttle_rate <- 5 #seconds
+retry_max_tries <- 3
+
+# Time zone
+tz <- "Brazil/East"
