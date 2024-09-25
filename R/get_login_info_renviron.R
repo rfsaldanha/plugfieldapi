29e8b4accf50a13a7c5b3e81229491041a781946
Raphael Saldanha <rfsaldanha@gmail.com>
2024-09-25 09:29:11 +0200

    Fix typo

--- R/get_login_info_renviron.R
+++ R/get_login_info_renviron.R
@@ -6,15 +6,15 @@ get_login_info_renviron <- function(){
   )
 
   if(res$password == ""){
-    cli::cli_abort("Pugfield username not provided and not found on renviron. Please provide it using the plugfield_username, plugfield_password and plugfield_x_api_key values on environ.")
+    cli::cli_abort("Plugfield username not provided and not found on renviron. Please provide it using the plugfield_username, plugfield_password and plugfield_x_api_key values on environ.")
   }
 
   if(res$username == ""){
-    cli::cli_abort("Pugfield password not provided and not found on renviron. Please provide it using the plugfield_username, plugfield_password and plugfield_x_api_key values on environ.")
+    cli::cli_abort("Plugfield password not provided and not found on renviron. Please provide it using the plugfield_username, plugfield_password and plugfield_x_api_key values on environ.")
   }
 
   if(res$x_api_key == ""){
-    cli::cli_abort("Pugfield x-api-key not provided and not found on renviron. Please provide it using the plugfield_username, plugfield_password and plugfield_x_api_key values on environ.")
+    cli::cli_abort("Plugfield x-api-key not provided and not found on renviron. Please provide it using the plugfield_username, plugfield_password and plugfield_x_api_key values on environ.")
   }
 
   return(res)
